ca2c3944f0bb5c3cd167804d617e05b6363e24d6
tryangel <1297226301@qq.com>
2016-09-07 23:51:19 +0800

    Add files via upload

---
+++ install_vimrc.sh
@@ -0,0 +1,10 @@
+cd ~/.vim_rc
+
+echo 'set runtimepath+=~/.vim_rc
+
+source ~/.vimrc_d/vimrcs/basic.vim
+source ~/.vimrc_d/vimrcs/plugins_config.vim
+
+' > ~/.vimrc
+
+echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
