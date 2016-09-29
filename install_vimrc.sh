a15c9544469405b11f2bd4749b16a42966b59ca1
tryangel <1297226301@qq.com>
2016-09-29 12:36:13 +0800

    Update install_vimrc.sh

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,11 +1,11 @@
-cd /root/.vimrc_d
+cd /home/.vimrc_d
 
 rm -f /etc/vim/vimrc
 
-echo 'set runtimepath+=/root/.vimrc_d
+echo 'set runtimepath+=/home/.vimrc_d
 
-source /root/.vimrc_d/vimrcs/basic.vim
-source /root/.vimrc_d/vimrcs/plugins_config.vim
+source /home/.vimrc_d/vimrcs/basic.vim
+source /home/.vimrc_d/vimrcs/plugins_config.vim
 
 ' > /etc/vim/vimrc
 
