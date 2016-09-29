879bdec403f1853a0741b53e28e0236e15e309ff
tryangel <1297226301@qq.com>
2016-09-29 13:07:35 +0800

    Create install_vimrc.sh

---
+++ install_vimrc.sh
@@ -0,0 +1,11 @@
+cd ~/.vimrc_d
+ 
+ echo 'set runtimepath+=~/.vimrc_d
+ 
+ source ~/.vimrc_d/vimrcs/basic.vim
+ source ~/.vimrc_d/vimrcs/plugins_config.vim
+ 
+ ' > ~/.vimrc
+ 
+ echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+
